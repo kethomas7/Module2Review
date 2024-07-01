1a5e86c8454335e7da4c7c53b62da1c3f786eb00
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-07-01 14:46:23 -0500

    added the calculate the avg grade method

--- Module2Review/Student.cs
+++ Module2Review/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +13,11 @@ namespace Module2Review
         public int ID { get; set; }
 
         public List<double> Grades { get; set; }
-        
-      
+
+
         //public List<char> Grades = new List<char> { 'A', 'B', 'C', 'D', 'F' };
 
-       
+
 
         public void AddGrade(double grade)
         {
@@ -24,9 +25,23 @@ namespace Module2Review
             Grades.Add(grade);
         }
 
-        public void AddGrade( params double[] grades)
+        public void AddGrade(params double[] grades)
         {
             Grades.AddRange(grades);
+        }
+        public double CalculateAverageGrade()
+        {
+
+
+            if (Grades.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Grades.Average();
+
+            }
         }
     }
 }
