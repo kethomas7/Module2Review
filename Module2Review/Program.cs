d4ca46d64a9bdc3fe9162fee8bafde69f699231b
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-07-01 14:35:39 -0500

    created student class, added 2 methods

---
+++ Module2Review/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Module2Review;
+
+Console.WriteLine("Hello, World!");
+
---
+++ Module2Review/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Review
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+        public List<double> Grades { get; set; }
+        
+      
+        //public List<char> Grades = new List<char> { 'A', 'B', 'C', 'D', 'F' };
+
+       
+
+        public void AddGrade(double grade)
+        {
+
+            Grades.Add(grade);
+        }
+
+        public void AddGrade( params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+    }
+}
